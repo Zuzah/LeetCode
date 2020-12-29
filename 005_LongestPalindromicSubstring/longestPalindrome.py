da3be4d284e25aa3d337ac841c9b24d0f6651ded
Zuzah <murtaza.hasni@gmail.com>
2020-12-29 14:44:04 -0500

    updated python version of question 005 longest palindromic string

--- 005_LongestPalindromicSubstring/longestPalindrome.py
+++ 005_LongestPalindromicSubstring/longestPalindrome.py
@@ -105,9 +105,9 @@ class Solution:
         left = 
     '''
 
-    def longestPalindrome(self, s: str) -> str:
+    def longestPalindromeUgly(self, s: str) -> str:
         # vars
-        myPalendromes = {}
+        myPalendromes = {} #len(word), palenddrome
         largestPalendrome: int = 0
         midpoint: int
         leftTuple: int
@@ -121,6 +121,7 @@ class Solution:
             return ""
   
         # if the string char # are event i.e 'aa' or 'aaaa'. Len = 2 or 4. .: tuples - 2/2=1 and 4/2=2. Tuples are 
+        
         if(isEvenLength == True):
             # determine the tuple chars
             leftTuple = s[int(len(s)/2)-1]
@@ -165,47 +166,164 @@ class Solution:
 
         return myPalendromes[largestPalendrome]
 
+    '''
+    Walkthrough:
+    s = racecar
+
+    - iterate along the string
+    - at each index i (i=0 is 'r')
+        - while (left >=0, right< len(s)):
+            - chick if left/right cars are equal (at beggining, both point to i[0])
+                if equal and if value > largestPalendrome
+                
+    '''
+
+    def longestPalindromeBetter(self, s: str) -> str:
+        '''
+        Approach: "expand around center" where centre is relative as you iterate along the array
+            i.e = s = 'racecar'
+            r | a | c | e | c | a | r
+
+            a | a | a | a
+
+        Requirments
+        - left =0, right = 0
+        - largestPalendrome = ""
+        - flag for isEven: isEvengt = True if len(s) % 2 == 0 else False
+        - while loop (left >=0 and right<len(s)):
+            if (s[left] == s[right]):
+                left-+1
+                right+=1
+            else:
+                break
+            
+            currentWord = s[left:right] #if odd?
+
+            largestPalendrom = currentWord if len(currentWord) > largestPalendrome else largestPalendrom
+
+        '''
+        leftIndex = 0
+        rightIndex = 0
+        left: int = 0
+        right: int = 0
+        currentWord = ""
+        center = 0
+        largestPalendrome = ""
+        isEven = True if len(s) % 2 == 0 else False
+
+        # test: abbc
+        for i, ichar in enumerate(s):
+            left = i
+            right = i
+            leftIndex = 0
+            rightIndex = 0
+            while (left >=0 and right < len(s)):
+                if (s[left] == s[right]):
+                    leftIndex = left
+                    rightIndex = right
+                else:
+                    break
+                left-=1
+                right+=1
+            currentWord = s[leftIndex: rightIndex+1]
+            print(currentWord)
+
+            if (len(currentWord) > len(largestPalendrome)):
+                largestPalendrome = currentWord
+
+        return largestPalendrome
+
+
+        
+
+    def longestPalindrome(self, s: str) -> str:
+
+        #need two method: helpder and longest palnedrom
+        # must consider cases if the substring is even or odd length
+        # must approach as relative to index: start at index -0. Check left, right pointesrs. if s[index]
+
+        '''
+        - odd/even check
+        'relative midpoint' -> iterate through s. check adjacent next cell (i+1). If exists, and IF ==, then even: get tuple. IF exists and not equal, odd: get midpoint
+            Case1: "racecar" -> r. midpoint = 0. left = 0, right=2. No, so next itera
+                                ra. midpoint = 1. left = 0, right = 2. len = 3. Not palendrome, 
+                                rac
+        '''
+
+        #vars: racecar
+        # raabbaacar
+        # calculate for odd palindrome
+        # for i in range(len(s)):
+        #   midpoint = i
+        #   len = 1
+        #   left = i - 1
+        #   right = i + 1
+        #   while (left >= 0 and right < len(s) && s[left] == s[right]):
+        #     len += 2
+        #     left -= 1
+        #     right += 1
+
+         # calculate for even palindrome
+        # for i in range(len(s)):
+        #   midpoint = i
+        #   if (s[midpoint] == s[midpoint + 1]):
+        #   len = 2
+        #   left = midpoint - 1
+        #   right = midpoint + 2
+        #   while (left >= 0 and right < len(s) && s[left] == s[right]):
+        #     len += 2
+        #     left -= 1
+        #     right += 1
+        
+
+        pass
+
 class SolutionTest(unittest.TestCase):
 
-    
+    # Test Cases for odd length strings
+    '''
     def test01 (self):
-        result = Solution().longestPalindrome("racecar")
+        result = Solution().longestPalindromeBetter("racecar")
         self.assertEqual(result, "racecar", "Expected Racecar")
-    
-    
-    def test02 (self):
-        result = Solution().longestPalindrome("")
-        self.assertEqual(result, "", "Expected empty")
-    
+
     def test03 (self):
-        result = Solution().longestPalindrome("a")
+        result = Solution().longestPalindromeBetter("a")
         self.assertEqual(result, "a", "Expected a")
     
     def test04 (self):
         self.lst = ['aba', 'bab']
-        result = Solution().longestPalindrome("babad")
+        result = Solution().longestPalindromeBetter("babad")
         self.assertIn(result, self.lst)
     
+    def test06 (self):
+        self.lst = ['a','c']
+        result = Solution().longestPalindromeBetter("abcda")
+        self.assertIn(result, self.lst)
+    '''
+    def test07 (self):
+        result = Solution().longestPalindromeBetter("abbc")
+        self.assertEqual(result, "bb", "Expected bb")
+    '''
+    def test02 (self):
+        result = Solution().longestPalindrome("")
+        self.assertEqual(result, "", "Expected empty")
+
     
+   
     def test05 (self):
         result = Solution().longestPalindrome("cbbd")
         self.assertEqual(result, "bb", "Expected bb")
     
-    def test06 (self):
-        self.lst = ['a,','c']
-        result = Solution().longestPalindrome("abcda")
-        self.assertIn(result, self.lst)
+
     
     def test06 (self):
         self.lst = ['a','c']
         result = Solution().longestPalindrome("ac")
         self.assertIn(result, self.lst)
     
-    def test07 (self):
-        result = Solution().longestPalindrome("abb")
-        self.assertEqual(result, "bb", "Expected bb")
+
     
-    
+    '''
 if __name__ == "__main__":
     unittest.main()
     #myObj = Solution()
